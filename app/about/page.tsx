9bba3fc5d034b3759351814c5284623f214b039f
Guido Vizoso <guido.vizoso9@gmail.com>
2023-12-21 18:11:07 -0300

    enhance about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,24 +15,33 @@ export default async function Home() {
       <h1 className="scroll-m-20 text-3xl font-bold tracking-tight mt-16">
         About
       </h1>
-      <div className="mt-16">
+      <ul className="mt-16 mx-0">
         {EXPERIENCE.map((exp) => {
           return (
-            <div key={exp.at} className="[&:not(:first-child)]:mt-16">
-              <h2 className="font-medium text-foreground/60">{exp.at}</h2>
+            <li
+              key={exp.at}
+              className="[&:not(:first-child)]:mt-12 list-none border-b border-border pb-4"
+            >
               <Link href={exp.atUrl} target="_blank">
-                <h3 className="scroll-m-20 text-lg font-semibold tracking-tight">
-                  {exp.title}
-                </h3>
+                <h2 className="font-medium text-foreground/60">{exp.date}</h2>
+              </Link>
+              <Link href={exp.atUrl} target="_blank">
+                <h2 className="scroll-m-20 text-lg font-semibold tracking-tight">
+                  {exp.title}{" "}
+                  <span className="text-foreground/60 font-light">@</span>{" "}
+                  <Link href={exp.atUrl} target="_blank">
+                    {exp.at}
+                  </Link>
+                </h2>
               </Link>
               <div
                 className="mt-4"
                 dangerouslySetInnerHTML={{ __html: exp.desc }}
               />
-            </div>
+            </li>
           );
         })}
-      </div>
+      </ul>
     </main>
   );
 }
