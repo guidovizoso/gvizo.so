255e7c11ceb2392d31148d525483fb56ac5a0d26
Guido Vizoso <guido.vizoso9@gmail.com>
2024-01-12 14:57:33 -0300

    change isProd approach

--- app/[slug]/stats.tsx
+++ app/[slug]/stats.tsx
@@ -1,19 +1,17 @@
-import { config } from "@/config";
 import { PostStat, getPostStats, incrementPostStat } from "@/lib/redis";
-import { headers } from "next/headers";
+
+const isProd = process?.env?.NODE_ENV === "production";
 
 export async function Stats({ slug }: { slug: string }) {
-  const fullUrl = headers().get("referer");
   const stats = await getPostStats(slug);
 
-  const isProd = fullUrl?.startsWith(config.domain);
   if (isProd) {
     incrementPostStat(slug, PostStat.Views);
   }
 
   return (
     <div className="flex flex-row gap-2 text-sm">
-      <span className="text-foreground/70">{stats.views}</span>
+      <span className="text-foreground/70">{stats.views}</span> stat
     </div>
   );
 }
