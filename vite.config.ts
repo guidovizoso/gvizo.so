7b1bbab1c088165b5d6cadb9ae7ea6c2bcd2eaaf
Guido Vizoso <guido.vizoso9@gmail.com>
2025-10-13 23:00:00 -0300

    nitro config

--- vite.config.ts
+++ vite.config.ts
@@ -8,52 +8,63 @@ import wasm from "vite-plugin-wasm";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		wasm(),
-		contentCollections(),
-		tsconfigPaths(),
-		tailwindcss(),
-		tanstackStart({
-			prerender: {
-				enabled: true,
-				crawlLinks: true,
-				filter: ({ path }) => {
-					// console.log(path);
-					// return path.includes("/posts");
-					return true;
-				},
-				onSuccess: ({ page }) => {
-					// biome-ignore lint/suspicious/noConsole: Safety check
-					console.log(`Rendered ${page.path}!`);
-				},
-			},
-		}),
-		nitro(),
-		viteReact(),
-	],
-	nitro: {
-		preset: "vercel",
-	},
-	optimizeDeps: {
-		exclude: [
-			"@takumi-rs/core",
-			"@takumi-rs/image-response",
-			"@takumi-rs/helpers",
-			"@takumi-rs/core-linux-x64-gnu",
-			"@takumi-rs/core-linux-arm64-gnu",
-			"@takumi-rs/core-darwin-arm64",
-			"@takumi-rs/core-darwin-x64",
-		],
-	},
-	ssr: {
-		external: [
-			"@takumi-rs/core",
-			"@takumi-rs/image-response",
-			"@takumi-rs/helpers",
-			"@takumi-rs/core-linux-x64-gnu",
-			"@takumi-rs/core-linux-arm64-gnu",
-			"@takumi-rs/core-darwin-arm64",
-			"@takumi-rs/core-darwin-x64",
-		],
-	},
+  plugins: [
+    wasm(),
+    contentCollections(),
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+        filter: ({ path }) => {
+          // console.log(path);
+          // return path.includes("/posts");
+          return true;
+        },
+        onSuccess: ({ page }) => {
+          // biome-ignore lint/suspicious/noConsole: Safety check
+          console.log(`Rendered ${page.path}!`);
+        },
+      },
+    }),
+    nitro(),
+    viteReact(),
+  ],
+  nitro: {
+    preset: "vercel",
+    externals: {
+      traceInclude: [
+        "node_modules/@takumi-rs/core",
+        "node_modules/@takumi-rs/image-response",
+        "node_modules/@takumi-rs/helpers",
+        "node_modules/@takumi-rs/core-linux-x64-gnu",
+        "node_modules/@takumi-rs/core-linux-arm64-gnu",
+        "node_modules/@takumi-rs/core-darwin-arm64",
+        "node_modules/@takumi-rs/core-darwin-x64",
+      ],
+    },
+  },
+  optimizeDeps: {
+    exclude: [
+      "@takumi-rs/core",
+      "@takumi-rs/image-response",
+      "@takumi-rs/helpers",
+      "@takumi-rs/core-linux-x64-gnu",
+      "@takumi-rs/core-linux-arm64-gnu",
+      "@takumi-rs/core-darwin-arm64",
+      "@takumi-rs/core-darwin-x64",
+    ],
+  },
+  ssr: {
+    external: [
+      "@takumi-rs/core",
+      "@takumi-rs/image-response",
+      "@takumi-rs/helpers",
+      "@takumi-rs/core-linux-x64-gnu",
+      "@takumi-rs/core-linux-arm64-gnu",
+      "@takumi-rs/core-darwin-arm64",
+      "@takumi-rs/core-darwin-x64",
+    ],
+  },
 });
